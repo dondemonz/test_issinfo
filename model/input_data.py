7a9877e8db1cb44d6b2cad77c7a8e5f33d604f2e
root <kykareky05@mail.ru>
2019-10-29 10:55:55 +0300

    24

--- model/input_data.py
+++ model/input_data.py
@@ -1,10 +1,10 @@
 keyword = 'SecurOS'
 path = r"C:\Program Files (x86)\ISS\SecurOS\tools\issinfo.exe"
-working_dirrectory = r"C:\Devel\issinfo_exe\tests\ISSInfo.7z"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
+working_dirrectory = r"C:\Devel\test_issinfo\tests\ISSInfo.7z"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
 # возможно, стоит запускать через системную переменную path
 dump_to_copy = r"C:\Devel\dumpfortest\video_full1.dmp"
 path_to_copy = r"C:\ProgramData\ISS\dumps\video_full1.dmp"
 window_title = r"C:\Devel\issinfo_exe\tests"
 path_to_7zip = r'C:\Program Files\7-Zip\7z.exe'
-path_to_archive = r'C:\Devel\issinfo_exe\tests\archive'
-path_to_postgress_logs = r"C:\Devel\issinfo_exe\tests\archive\ISSInfo\PostgresLogs"
+path_to_archive = r'C:\Devel\test_issinfo\tests\archive'
+path_to_postgress_logs = r"C:\Devel\test_issinfo\tests\archive\ISSInfo\PostgresLogs"
