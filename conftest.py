4b286bfaca127ac51f6e1cc7a71af68f6db34c02
root <kykareky05@mail.ru>
2019-10-29 10:16:49 +0300

    24

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import time
+import os
+import shutil
+
+
+@pytest.fixture(scope="session", autouse=True)
+def fix(request):
+    os.system('taskkill /f /im securos_svc.exe')
+    shutil.rmtree(r'C:\ProgramData\ISS\logs', ignore_errors=True)
+    time.sleep(5)
+
+    def fin():
+        print('\nSome resource fin')
+
+    request.addfinalizer(fin)
