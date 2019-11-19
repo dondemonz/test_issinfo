d50c10f73d1c1906835fb66b23eb9514d36fce19
root <kykareky05@mail.ru>
2019-11-19 08:05:26 +0300

    24

--- tests/test_issinfo.py
+++ tests/test_issinfo.py
@@ -125,7 +125,9 @@ def test_login_client():
     # так как фикстура запускает перезапуск процесса (возможно, стоит над этим подумать и как-то изменить), нужен слип, чтобы вроцесс запустился
     time.sleep(25)
     app1 = Application(backend="uia").connect(title="SecurOS Enterprise")
+    time.sleep(1)
     app1.window_().Edit2.type_keys("securos")
+    time.sleep(1)
     app1.window_().Авторизоваться.click()
     #app1.window().print_control_identifiers()
 
