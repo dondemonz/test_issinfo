0e9a23de4447f8d7f8ab8aa6fe668ddf0e8de3ae
root <kykareky05@mail.ru>
2020-01-12 22:52:37 +0300

    24

--- model/input_data.py
+++ model/input_data.py
@@ -1,5 +1,5 @@
 keyword = 'SecurOS'
-path = "C:\\Program Files (x86)\\ISS\\SecurOS\\tools\\issinfo.exe"
+path = r"C:\Program Files (x86)\ISS\SecurOS\1tools\issinfo.exe"
 working_dirrectory = r"C:\Devel\test_issinfo\tests\ISSInfo.7z"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
 # возможно, стоит запускать через системную переменную path
 dump_to_copy = r"C:\Devel\dumpfortest\video_full1.dmp"
