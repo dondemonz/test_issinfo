38683049ea732fd732269400c18f851b5c60533b
root <kykareky05@mail.ru>
2020-01-13 13:07:58 +0300

    24

--- conftest.py
+++ conftest.py
@@ -6,9 +6,9 @@ import shutil
 
 @pytest.fixture(scope="session")
 def fix(request):
-    #os.system('taskkill /f /im securos_svc.exe')
-    #shutil.rmtree(r'C:\ProgramData\ISS\logs', ignore_errors=True)
-    #shutil.rmtree(r'C:\ProgramData\ISS\dumps', ignore_errors=True)
+    os.system('taskkill /f /im securos_svc.exe')
+    shutil.rmtree(r'C:\ProgramData\ISS\logs', ignore_errors=True)
+    shutil.rmtree(r'C:\ProgramData\ISS\dumps', ignore_errors=True)
     time.sleep(5)
 
     def fin():
