58dd763e733f2eb0d30373bbc100b0dbc4fd778c
root <kykareky05@mail.ru>
2020-01-13 15:35:49 +0300

    24

--- tests/test_issinfo.py
+++ tests/test_issinfo.py
@@ -124,6 +124,7 @@ def test_size_of_postgress_logs():
             total_size += os.path.getsize(fp)
     print("Directory size: " + str(total_size))
     assert total_size < 1000000
+    time.sleep(1)
     #shutil.rmtree(path_to_archive)
 
 #тест сделан для того, чтобы другие тесты не ломались без залогиненного клиента
