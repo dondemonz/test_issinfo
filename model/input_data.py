72b8be7852a6aaea50a7f2d4470fcb050c3476a0
root <kykareky05@mail.ru>
2020-01-14 20:13:13 +0300

    24

--- model/input_data.py
+++ model/input_data.py
@@ -1,7 +1,7 @@
-pc_name="VQA-2_"
+pc_name = "VQA-2"
 keyword = 'SecurOS'
 path = r"C:\Program Files (x86)\ISS\SecurOS\tools\issinfo.exe"
-working_dirrectory = r"C:\Devel\test_issinfo\test\ISSInfo.7z"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
+working_dirrectory = r"C:\Devel\test_issinfo\test"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
 # возможно, стоит запускать через системную переменную path
 dump_to_copy = r"C:\Devel\dumpfortest\video_full1.dmp"
 path_to_copy = r"C:\ProgramData\ISS\dumps\video_full1.dmp"
@@ -9,5 +9,5 @@ window_title = r"C:\Devel\issinfo_exe\test"
 path_to_7zip = r'C:\Program Files\7-Zip\7z.exe'
 path_to_archive = r'C:\Devel\test_issinfo\test\archive'
 path_to_postgress_logs = r"C:\Devel\test_issinfo\test\archive\ISSInfo\PostgresLogs"
-# working_dirrectory_jenkins = r"C:\Users\root\.jenkins\workspace\tests-issinfo\ISSInfo.7z"
+working_dirrectory_jenkins = r"C:\Users\root\.jenkins\workspace\tests-issinfo\ISSInfo.7z"
 working_dirrectory_jenkins_as_service = r"c:\workspace\tests-issinfo\ISSInfo_"
