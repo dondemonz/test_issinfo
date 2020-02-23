6755fd387fb5da00e160b494f2c71cad37498a3c
root <kykareky05@mail.ru>
2020-02-23 17:24:08 +0300

    24

--- model/input_data.py
+++ model/input_data.py
@@ -1,7 +1,7 @@
 pc_name = "VQA-2_"
 keyword = 'SecurOS'
-path = r"C:\Program Files (x86)\ISS\SecurOS\tools\issinfo.exe"
-working_dirrectory = r"C:\Devel\test_issinfo\test"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
+path = r"C:\Program Files (x86)\ISS\SecurOS\tools\SystemInfo.exe"
+working_dirrectory = r"C:\Devel\test_issinfo\test\SystemInfo_"  #Как сделать так, чтобы iisinfo запускался из C:\\Program Files (x86)... я пока не понял, но это может сломать проверку в дженкинсе
 # возможно, стоит запускать через системную переменную path
 dump_to_copy = r"C:\Devel\dumpfortest\video_full1.dmp"
 path_to_copy = r"C:\ProgramData\ISS\dumps\video_full1.dmp"
@@ -10,4 +10,4 @@ path_to_7zip = r'C:\Program Files\7-Zip\7z.exe'
 path_to_archive = r'C:\workspace\tests-issinfo\archive'
 path_to_postgress_logs = r'C:\workspace\tests-issinfo\archive'
 #working_dirrectory_jenkins = r"C:\Users\root\.jenkins\workspace\tests-issinfo\ISSInfo.7z"
-working_dirrectory_jenkins_as_service = r"c:\workspace\tests-issinfo\ISSInfo_"
+working_dirrectory_jenkins_as_service = r"C:\workspace\tests-issinfo\SystemInfo_"
--- test/test_issinfo.py
+++ test/test_issinfo.py
@@ -23,14 +23,16 @@ def test_only_full_dumps(fix):
     app = Application(backend="uia").start(path)
     file_name = working_dirrectory_jenkins_as_service+pc_name+tm+".7z"
     file_name1 = working_dirrectory_jenkins_as_service+pc_name+tm1+".7z"
+    file_name2 = working_dirrectory+pc_name+tm+".7z"
+    file_name3 = working_dirrectory+pc_name+tm1+".7z"
     time.sleep(10)
-    app.connect(title='ISSInfo')
+    app.connect(title='SystemInfo Utility')
     #app = Application().connect(title='Server Control Agent')
-    dlg = app.window(title='ISSInfo')
+    dlg = app.window(title='SystemInfo Utility')
     dlg1 = dlg.child_window(auto_id="1003")
     value = dlg1.get_value()
     time.sleep(2)
-    assert value == file_name or value == file_name1
+    assert value == file_name or value == file_name1 or value == file_name2 or value == file_name3
     #print("connected")
     #проверка чек-боксов
     dlg2 = dlg.child_window(auto_id="1009")
@@ -42,8 +44,9 @@ def test_only_full_dumps(fix):
     dlg4 = dlg.child_window(auto_id="1010")
     value4 = dlg4.get_toggle_state()
     assert value4 == 0
+    time.sleep(3)
     #изменение имени файла с нового с именем компа и датой на старое ISSInfo.7z для удобства, возможно позже надо будет сделать проверку
-    dlg5 = dlg.child_window(auto_id="1003")
+    #dlg5 = dlg.child_window(auto_id="1003")
     #dlg5.print_ctrl_ids
     dlg.Пуск.click()
     #dlg.child_window(auto_id="1001").click()
@@ -56,22 +59,34 @@ def test_only_full_dumps(fix):
     new_dlg.Открытьдиректорию.click()
     time.sleep(1)
     # разные воркспейсы у дженкинса и пайчарма осложняют жизнь
-    app1 = Application().connect(title=r"C:\workspace\tests-issinfo")
+    #app1 = Application().connect(title="SystemInfo Utility")
     #except pywinauto.findwindows.ElementNotFoundError:
-    #app1 = Application().connect(title="C:\\Devel\\test_issinfo\\tests")
-    window = app1.window(title=r"C:\workspace\tests-issinfo")
+    app = Application().connect(title=r"C:\Devel\test_issinfo\test")
+    window = app.window(title=r"C:\Devel\test_issinfo\test")
     time.sleep(1)
     window.close()
     #new_dlg.OK.Click()
+    delete_issinfo(file_name, file_name1, file_name2, file_name3)
+    dlg.close()
+
+
+def delete_issinfo(file_name, file_name1, file_name2, file_name3):
     if os.path.isfile(file_name):
         f = os.path.isfile(file_name)
         assert f == True
         os.remove(file_name)
-    else:
+    elif os.path.isfile(file_name1):
         f = os.path.isfile(file_name1)
         assert f == True
         os.remove(file_name1)
-    dlg.close()
+    elif os.path.isfile(file_name2):
+        f = os.path.isfile(file_name2)
+        assert f == True
+        os.remove(file_name2)
+    else:
+        f = os.path.isfile(file_name3)
+        assert f == True
+        os.remove(file_name3)
 
 
 def test_delete_dumps():
@@ -79,11 +94,13 @@ def test_delete_dumps():
     m1 = m + timedelta(seconds=1)
     tm = m.strftime("%Y.%m.%d_%H.%M.%S")
     tm1 = m1.strftime("%Y.%m.%d_%H.%M.%S")
-    app = Application(backend="uia").start(path).connect(title='ISSInfo')
-    file_name2 = working_dirrectory_jenkins_as_service+pc_name+tm+".7z"
-    file_name3 = working_dirrectory_jenkins_as_service+pc_name+tm1+".7z"
+    app = Application(backend="uia").start(path).connect(title='SystemInfo Utility')
+    file_name = working_dirrectory_jenkins_as_service+pc_name+tm+".7z"
+    file_name1 = working_dirrectory_jenkins_as_service+pc_name+tm1+".7z"
+    file_name2 = working_dirrectory+pc_name+tm+".7z"
+    file_name3 = working_dirrectory+pc_name+tm1+".7z"
     #app = Application(backend="uia").connect(title='ISSInfo')
-    dlg = app.window(title='ISSInfo')
+    dlg = app.window(title='SystemInfo Utility')
     dlg2 = dlg.child_window(auto_id="1011")
     #как именно выделять чек-бокс, не разобрался. Просто кликаю, ставит\снимает.
     dlg2.click()
@@ -95,14 +112,7 @@ def test_delete_dumps():
     new_dlg = app.top_window()
     new_dlg.OK.click()
     dlg.close()
-    if os.path.isfile(path_to_copy):
-        pytest.fail("File is not deleted")
-    os.path.exists(path_to_copy)
-    print("File is deleted")
-    if os.path.isfile(file_name2):
-        os.remove(file_name2)
-    else:
-        os.remove(file_name3)
+    delete_issinfo(file_name, file_name1, file_name2, file_name3)
 
 def test_additional_databases():
     #if os.path.isfile(r'C:\workspace\tests-issinfo\ISSInfo.7z'):
@@ -111,11 +121,13 @@ def test_additional_databases():
     m1 = m + timedelta(seconds=1)
     tm = m.strftime("%Y.%m.%d_%H.%M.%S")
     tm1 = m1.strftime("%Y.%m.%d_%H.%M.%S")
-    app = Application(backend="uia").start(path).connect(title='ISSInfo')
-    file_name4 = working_dirrectory_jenkins_as_service+pc_name+tm+".7z"
-    file_name5 = working_dirrectory_jenkins_as_service+pc_name+tm1+".7z"
+    app = Application(backend="uia").start(path).connect(title='SystemInfo Utility')
+    file_name = working_dirrectory_jenkins_as_service+pc_name+tm+".7z"
+    file_name1 = working_dirrectory_jenkins_as_service+pc_name+tm1+".7z"
+    file_name2 = working_dirrectory+pc_name+tm+".7z"
+    file_name3 = working_dirrectory+pc_name+tm1+".7z"
      # app = Application(backend="uia").connect(title='ISSInfo')
-    dlg = app.window(title='ISSInfo')
+    dlg = app.window(title='SystemInfo Utility')
     dlg2 = dlg.child_window(auto_id="1010")
     # как именно выделять чек-бокс, не разобрался. Просто кликаю, ставит\снимает.
     dlg2.click()
@@ -133,19 +145,7 @@ def test_additional_databases():
         copyfile(file_name5, working_dirrectory)
     """
 
-    # проверка, есть ли доп. база postgres в issinfo
-    if os.path.isfile(file_name4):
-        p = Popen(path_to_7zip + ' l ' + file_name4, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
-    else:
-        p = Popen(path_to_7zip + ' l ' + file_name5, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
+    d = check_if_db_postgres_in_issinfo(file_name, file_name1, file_name2, file_name3)
 
     if not "protocol.sql" in d:
         pytest.fail("protocol.sql is not in issinfo")
@@ -156,32 +156,17 @@ def test_additional_databases():
         os.makedirs(path_to_archive)
     time.sleep(2)
 
-    if os.path.isfile(file_name4):
-        patoolib.extract_archive(file_name4, outdir=path_to_archive)
-    else:
-        patoolib.extract_archive(file_name5, outdir=path_to_archive)
+    extract_files_from_issinfo(file_name, file_name1, file_name2, file_name3)
     time.sleep(15)
     total_size = 0
 
-    if os.path.isfile(file_name4):
-        for path1, dirs, files in os.walk(path_to_postgress_logs+file_name4+"\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
-    else:
-        for path1, dirs, files in os.walk(path_to_postgress_logs+file_name5+"\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
+    total_size = check_size_of_postgres_logs(file_name, file_name1, file_name2, file_name3, total_size)
 
     print("Directory size: " + str(total_size))
     assert total_size < 1000000
     time.sleep(1)
     shutil.rmtree(path_to_archive)
-    if os.path.isfile(file_name4):
-        os.remove(file_name4)
-    else:
-        os.remove(file_name5)
+    delete_issinfo(file_name, file_name1, file_name2, file_name3)
 
 #тест сделан для того, чтобы другие тесты не ломались без залогиненного клиента
 def test_login_client():
@@ -194,6 +179,72 @@ def test_login_client():
     time.sleep(1)
     app1.window().Авторизоваться.click()
     #app1.window().print_control_identifiers()
+
+def check_size_of_postgres_logs(file_name, file_name1, file_name2, file_name3, total_size):
+    if os.path.isfile(file_name):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    elif os.path.isfile(file_name1):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name1 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    elif os.path.isfile(file_name2):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name2 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    else:
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name3 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    return total_size
+
+
+def extract_files_from_issinfo(file_name, file_name1, file_name2, file_name3):
+    if os.path.isfile(file_name):
+        patoolib.extract_archive(file_name, outdir=path_to_archive)
+    if os.path.isfile(file_name1):
+        patoolib.extract_archive(file_name1, outdir=path_to_archive)
+    if os.path.isfile(file_name2):
+        patoolib.extract_archive(file_name2, outdir=path_to_archive)
+    else:
+        patoolib.extract_archive(file_name3, outdir=path_to_archive)
+
+
+def check_if_db_postgres_in_issinfo(file_name, file_name1, file_name2, file_name3):
+    # проверка, есть ли доп. база postgres в issinfo
+    if os.path.isfile(file_name):
+        p = Popen(path_to_7zip + ' l ' + file_name, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    elif os.path.isfile(file_name1):
+        p = Popen(path_to_7zip + ' l ' + file_name1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    elif os.path.isfile(file_name2):
+        p = Popen(path_to_7zip + ' l ' + file_name2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    else:
+        p = Popen(path_to_7zip + ' l ' + file_name3, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    return d
+
+
+
 
 
 """
