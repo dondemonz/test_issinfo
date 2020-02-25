9a8299a8b223dfa45b58cc00ee5b231cc39b5ca6
root <kykareky05@mail.ru>
2020-02-25 23:34:33 +0300

    24

---
+++ fixture/helper.py
@@ -0,0 +1,102 @@
+from datetime import timedelta
+from model.input_data import *
+import os
+import patoolib
+from subprocess import Popen, PIPE
+
+def set_file_name_with_datetime(m):
+    m1 = m + timedelta(seconds=1)
+    tm = m.strftime("%Y.%m.%d_%H.%M.%S")
+    tm1 = m1.strftime("%Y.%m.%d_%H.%M.%S")
+    file_name = working_dirrectory_jenkins_as_service + pc_name + tm + ".7z"
+    file_name1 = working_dirrectory_jenkins_as_service + pc_name + tm1 + ".7z"
+    file_name2 = working_dirrectory + pc_name + tm + ".7z"
+    file_name3 = working_dirrectory + pc_name + tm1 + ".7z"
+    return file_name, file_name1, file_name2, file_name3
+
+def close_final_dialogs(app, dlg):
+    new_dlg = app.top_window()
+    new_dlg.OK.click()
+    dlg.close()
+
+def delete_issinfo(file_name, file_name1, file_name2, file_name3):
+    if os.path.isfile(file_name):
+        f = os.path.isfile(file_name)
+        assert f == True
+        os.remove(file_name)
+    elif os.path.isfile(file_name1):
+        f = os.path.isfile(file_name1)
+        assert f == True
+        os.remove(file_name1)
+    elif os.path.isfile(file_name2):
+        f = os.path.isfile(file_name2)
+        assert f == True
+        os.remove(file_name2)
+    else:
+        f = os.path.isfile(file_name3)
+        assert f == True
+        os.remove(file_name3)
+
+def check_size_of_postgres_logs(file_name, file_name1, file_name2, file_name3, total_size):
+    if os.path.isfile(file_name):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    elif os.path.isfile(file_name1):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name1 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    elif os.path.isfile(file_name2):
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name2 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    else:
+        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name3 + "\PostgresLogs"):
+            for f in files:
+                fp = os.path.join(path1, f)
+                total_size += os.path.getsize(fp)
+    return total_size
+
+
+def extract_files_from_issinfo(file_name, file_name1, file_name2, file_name3):
+    if os.path.isfile(file_name):
+        patoolib.extract_archive(file_name, outdir=path_to_archive)
+    elif os.path.isfile(file_name1):
+        patoolib.extract_archive(file_name1, outdir=path_to_archive)
+    elif os.path.isfile(file_name2):
+        patoolib.extract_archive(file_name2, outdir=path_to_archive)
+    else:
+        patoolib.extract_archive(file_name3, outdir=path_to_archive)
+
+
+
+def check_if_db_postgres_in_issinfo(file_name, file_name1, file_name2, file_name3):
+    # проверка, есть ли доп. база postgres в issinfo
+    if os.path.isfile(file_name):
+        p = Popen(path_to_7zip + ' l ' + file_name, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    elif os.path.isfile(file_name1):
+        p = Popen(path_to_7zip + ' l ' + file_name1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    elif os.path.isfile(file_name2):
+        p = Popen(path_to_7zip + ' l ' + file_name2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    else:
+        p = Popen(path_to_7zip + ' l ' + file_name3, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+        # d - список всех файлов в issinfo
+        d = output.decode('utf-8')
+    return d
--- test/test_issinfo.py
+++ test/test_issinfo.py
@@ -1,15 +1,12 @@
 from pywinauto.application import Application
-from model.input_data import *
+from fixture.helper import *
 import time
 import os
 from shutil import copyfile
 import pytest
-from subprocess import Popen, PIPE
 from parse import *
 import shutil
-import patoolib
 import datetime as dt
-from datetime import timedelta
 
 
 def test_only_full_dumps(fix):
@@ -44,6 +41,8 @@ def test_only_full_dumps(fix):
     # воркспейс для дженкинса, в пайчареме надо заменить на devel\
     app = Application().connect(title=r"C:\workspace\tests-issinfo")
     window = app.window(title=r"C:\workspace\tests-issinfo")
+    #app = Application().connect(title=r"C:\Devel\test_issinfo\test")
+    #window = app.window(title=r"C:\Devel\test_issinfo\test")
     time.sleep(1)
     window.close()
     delete_issinfo(file_name, file_name1, file_name2, file_name3)
@@ -96,118 +95,6 @@ def test_additional_databases():
     time.sleep(1)
     shutil.rmtree(path_to_archive)
     delete_issinfo(file_name, file_name1, file_name2, file_name3)
-
-"""
-#тест сделан для того, чтобы другие тесты не ломались без залогиненного клиента
-def test_login_client():
-    time.sleep(5)
-    # так как фикстура запускает перезапуск процесса (возможно, стоит над этим подумать и как-то изменить), нужен слип, чтобы процесс запустился
-    app1 = Application(backend="uia").connect(title="SecurOS Enterprise")
-    time.sleep(1)
-    app1.window().Edit2.click_input()
-    time.sleep(1)
-    app1.window().Edit2.type_keys("securos")
-    time.sleep(1)
-    app1.window().Авторизоваться.click()
-"""
-def close_final_dialogs(app, dlg):
-    new_dlg = app.top_window()
-    new_dlg.OK.click()
-    dlg.close()
-
-def set_file_name_with_datetime(m):
-    m1 = m + timedelta(seconds=1)
-    tm = m.strftime("%Y.%m.%d_%H.%M.%S")
-    tm1 = m1.strftime("%Y.%m.%d_%H.%M.%S")
-    file_name = working_dirrectory_jenkins_as_service + pc_name + tm + ".7z"
-    file_name1 = working_dirrectory_jenkins_as_service + pc_name + tm1 + ".7z"
-    file_name2 = working_dirrectory + pc_name + tm + ".7z"
-    file_name3 = working_dirrectory + pc_name + tm1 + ".7z"
-    return file_name, file_name1, file_name2, file_name3
-
-def delete_issinfo(file_name, file_name1, file_name2, file_name3):
-    if os.path.isfile(file_name):
-        f = os.path.isfile(file_name)
-        assert f == True
-        os.remove(file_name)
-    elif os.path.isfile(file_name1):
-        f = os.path.isfile(file_name1)
-        assert f == True
-        os.remove(file_name1)
-    elif os.path.isfile(file_name2):
-        f = os.path.isfile(file_name2)
-        assert f == True
-        os.remove(file_name2)
-    else:
-        f = os.path.isfile(file_name3)
-        assert f == True
-        os.remove(file_name3)
-
-def check_size_of_postgres_logs(file_name, file_name1, file_name2, file_name3, total_size):
-    if os.path.isfile(file_name):
-        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name + "\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
-    elif os.path.isfile(file_name1):
-        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name1 + "\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
-    elif os.path.isfile(file_name2):
-        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name2 + "\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
-    else:
-        for path1, dirs, files in os.walk(path_to_postgress_logs + file_name3 + "\PostgresLogs"):
-            for f in files:
-                fp = os.path.join(path1, f)
-                total_size += os.path.getsize(fp)
-    return total_size
-
-
-def extract_files_from_issinfo(file_name, file_name1, file_name2, file_name3):
-    if os.path.isfile(file_name):
-        patoolib.extract_archive(file_name, outdir=path_to_archive)
-    elif os.path.isfile(file_name1):
-        patoolib.extract_archive(file_name1, outdir=path_to_archive)
-    elif os.path.isfile(file_name2):
-        patoolib.extract_archive(file_name2, outdir=path_to_archive)
-    else:
-        patoolib.extract_archive(file_name3, outdir=path_to_archive)
-
-
-
-def check_if_db_postgres_in_issinfo(file_name, file_name1, file_name2, file_name3):
-    # проверка, есть ли доп. база postgres в issinfo
-    if os.path.isfile(file_name):
-        p = Popen(path_to_7zip + ' l ' + file_name, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
-    elif os.path.isfile(file_name1):
-        p = Popen(path_to_7zip + ' l ' + file_name1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
-    elif os.path.isfile(file_name2):
-        p = Popen(path_to_7zip + ' l ' + file_name2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
-    else:
-        p = Popen(path_to_7zip + ' l ' + file_name3, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-        rc = p.returncode
-        # d - список всех файлов в issinfo
-        d = output.decode('utf-8')
-    return d
-
-
 
 
 
